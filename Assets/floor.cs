92b35910f719042d7aeac9a3cecaee81e1ca1864
kwangdonlaya <donlaya.929@mail.kmutt.ac.th>
2020-06-01 03:10:54 +0700

    Rungame

---
+++ Assets/floor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floor : MonoBehaviour
+{
+    public float bgSpeed;
+    float bgPositionX;
+    float bg1PositionX;
+    // Use this for initialization
+    void Start()
+    {
+        bgPositionX = transform.position.x;
+        bg1PositionX = GameObject.Find("floor1").transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x + bgSpeed, transform.position.y, transform.position.z);
+        if (transform.position.x < (bgPositionX * -1f))
+        {
+            transform.position = new Vector3(bg1PositionX, transform.position.y, transform.position.z);
+        }
+    }
+
+
+}
---
+++ Assets/girl.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class girl : MonoBehaviour
+{
+    public float jumpSpeed;
+    Rigidbody2D rb;
+    Animator am;
+    int jump;
+    //Use this for initialzation
+    void Start()
+    {
+        jump = 0;
+        rb = GetComponent<Rigidbody2D>();
+        am = GetComponent<Animator>();
+    }
+    //Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && jump < 2)
+        {
+            jump++;
+            am.SetBool("Jump", true);
+            rb.velocity = new Vector2(rb.velocity.x, 5f);
+            
+        }
+        if (Input.GetButtonUp("Jump"))
+        {
+            am.SetBool("Jump", false);
+        }
+    }
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        am.SetBool("jump", false);
+        jump = 0;
+    }
+}
+    
+
+
